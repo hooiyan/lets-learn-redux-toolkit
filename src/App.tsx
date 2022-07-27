29be93a0509ea62bbc2a6a12b8952523eed14242
hooiyan <hoecoding@gmail.com>
2022-07-27 15:38:13 +0800

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,9 @@ function App() {
   const dispatch = useAppDispatch()
   const count = useAppSelector((state) => state.counter.value)
 
+  const [numDogs, setNumDogs] = useState(10)
+  const { data = [], isFetching } = useFetchBreedQuery(numDogs)
+
   function handleClick() {
     // dispatch(incremented())
     dispatch(amountAdded(10))
@@ -27,9 +30,41 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={handleClick}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+        <div>
+          <p>Dogs to fetch:</p>
+          <select
+            value={numDogs}
+            onChange={(e) => setNumDogs(Number(e.target.value))}
+          >
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="15">15</option>
+            <option value="20">20</option>
+          </select>
+        </div>
+
+        <div>
+          <p>Number of dogs fetched: {data.length}</p>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Picture</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((breed) => (
+                <tr key={breed.id}>
+                  <td>{breed.name}</td>
+                  <td>
+                    <img src={breed.image.url} alt={breed.name} height="250" />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
